1ded5304f5f5940040baa36de3f3a98e25b36b4c
soonable@naver.com <soonable@naver.com>
2023-03-28 19:06:55 +0900

    refactor : Response 시 AllArgsConstructor 필요

--- src/main/java/com/e1i4/sanmonkeybackend/dto/LoginReqDto.java
+++ src/main/java/com/e1i4/sanmonkeybackend/dto/LoginReqDto.java
@@ -1,8 +1,10 @@
 package com.e1i4.sanmonkeybackend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginReqDto {
     private String emailId;
     private String password;
