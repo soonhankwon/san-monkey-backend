a21f207be88bed4411b60557b734af973e5a093e
soonable@naver.com <soonable@naver.com>
2023-04-04 18:19:36 +0900

    feat : 에러코드 추가

--- src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
+++ src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    EMAIL_WITH_INVALID_EXPRESSION(HttpStatus.BAD_REQUEST, "이메일은 @와 .이 포함되어야 합니다.");
+    EMAIL_WITH_INVALID_EXPRESSION(HttpStatus.BAD_REQUEST, "이메일은 @와 .이 포함되어야 합니다."),
+    PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "잘못된 패스워드 입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
