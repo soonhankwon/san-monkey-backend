652cf7e075855e7c89f09d0bf79eb4074dfa7829
soonable@naver.com <soonable@naver.com>
2023-04-04 18:05:26 +0900

    feat : StampService 비즈니스 로직 (proto)

--- src/main/java/com/e1i4/sanmonkeybackend/service/StampService.java
+++ src/main/java/com/e1i4/sanmonkeybackend/service/StampService.java
@@ -1,16 +1,21 @@
 package com.e1i4.sanmonkeybackend.service;
 
 import com.e1i4.sanmonkeybackend.domain.Stamp;
-import com.e1i4.sanmonkeybackend.exception.ErrorCode;
+import com.e1i4.sanmonkeybackend.domain.User;
+import com.e1i4.sanmonkeybackend.domain.UserStamp;
 import com.e1i4.sanmonkeybackend.repository.StampRepository;
+import com.e1i4.sanmonkeybackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class StampService {
     private final StampRepository stampRepository;
-    public void createStampToDB(String stampImageUrl) {
+    private final UserRepository userRepository;
+
+    public void addStampToDB(String stampImageUrl) {
         stampRepository.save(new Stamp(stampImageUrl));
     }
 
@@ -18,5 +23,14 @@ public class StampService {
         Stamp stamp = stampRepository.findById(id)
                 .orElseThrow(IllegalArgumentException::new);
         return stamp.getStampImageUrl();
+    }
+
+    @Transactional
+    public void giveStampToUser(String email, Long stampId) {
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(IllegalArgumentException::new);
+        Stamp stamp = stampRepository.findById(stampId)
+                .orElseThrow(IllegalArgumentException::new);
+        user.updateStamp(new UserStamp(user, stamp));
     }
 }
