5287af387f1624806f647ceecda206fbb284541b
soonable@naver.com <soonable@naver.com>
2023-04-04 16:44:29 +0900

    feat : BaseTimeEntity

---
+++ src/main/java/com/e1i4/sanmonkeybackend/utils/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.e1i4.sanmonkeybackend.utils;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
