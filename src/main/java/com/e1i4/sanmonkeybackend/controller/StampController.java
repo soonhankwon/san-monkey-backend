e2cd4a8c9b788a3de60e2a92f2912c4438de2c42
soonable@naver.com <soonable@naver.com>
2023-04-06 18:13:53 +0900

    feat : url 변경 및 유저 스탬프 조회

--- src/main/java/com/e1i4/sanmonkeybackend/controller/StampController.java
+++ src/main/java/com/e1i4/sanmonkeybackend/controller/StampController.java
@@ -1,12 +1,16 @@
 package com.e1i4.sanmonkeybackend.controller;
 
+import com.e1i4.sanmonkeybackend.dto.GiveStampToUserReqDto;
+import com.e1i4.sanmonkeybackend.dto.StampReqDto;
+import com.e1i4.sanmonkeybackend.dto.UserStampReqDto;
+import com.e1i4.sanmonkeybackend.dto.UserStampResDto;
 import com.e1i4.sanmonkeybackend.service.StampService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -16,20 +20,20 @@ public class StampController {
     private final StampService stampService;
 
     @Operation(summary = "DB 스탬프 저장")
-    @PostMapping("/create/stamp")
-    public void createStamp(@RequestParam String stampImageUrl) {
-        stampService.addStampToDB(stampImageUrl);
+    @PostMapping("/db/stamp")
+    public void createStamp(StampReqDto stampReqDto) {
+        stampService.addStampToDB(stampReqDto);
     }
 
-    @Operation(summary = "스탬프 조회")
-    @GetMapping("/stamp")
-    public String getStamp(@RequestParam Long id) {
-        return stampService.getStamp(id);
+    @Operation(summary = "유저 스탬프 조회")
+    @GetMapping("/user/stamp")
+    public ResponseEntity<UserStampResDto> getStamp(UserStampReqDto userStampReqDto) {
+        return stampService.getUserStamp(userStampReqDto);
     }
 
-    @Operation(summary = "유저에게 스템프 수여")
-    @PostMapping("/user/stamp")
-    public void giveStampToUser(@RequestParam String email, Long stampId) {
-        stampService.giveStampToUser(email, stampId);
+    @Operation(summary = "유저 스템프 수여")
+    @PostMapping("/user/stamp/give")
+    public void giveStampToUser(GiveStampToUserReqDto giveStampToUserReqDto) {
+        stampService.giveStampToUser(giveStampToUserReqDto);
     }
 }
