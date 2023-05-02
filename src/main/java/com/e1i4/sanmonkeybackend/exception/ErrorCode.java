176dc6aa400524f0be913d213abc59618c21168c
soonable@naver.com <soonable@naver.com>
2023-05-02 16:25:30 +0900

    feat : 비밀번호 변경시 기존 비밀번호와 동일

--- src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
+++ src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "잘못된 패스워드 입니다."),
     LOGIN_INVALID_VALUE(HttpStatus.BAD_REQUEST, "이메일 또는 패스워드가 일치하지 않습니다."),
     EXISTS_USER_BY_EMAIL(HttpStatus.BAD_REQUEST, "가입된 이메일이 존재합니다."),
-    USERID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 아이디가 존재하지 않습니다.");
+    USERID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 아이디가 존재하지 않습니다."),
+    SAME_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호와 동일한 비밀번호 입니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
