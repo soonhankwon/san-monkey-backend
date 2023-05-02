cdb22901dec0fbf02dae474712f712d7679f68c8
soonable@naver.com <soonable@naver.com>
2023-05-02 11:48:20 +0900

    test : 테스트 변수 이름 수정

--- src/test/java/com/e1i4/sanmonkeybackend/domain/UserTest.java
+++ src/test/java/com/e1i4/sanmonkeybackend/domain/UserTest.java
@@ -12,23 +12,25 @@ public class UserTest {
     @Test
     @DisplayName("유저 객체 생성 테스트")
     void createUser() {
+        String userId = "soonId";
         String nickname = "soon";
         String password = "1234";
         String email = "soon@test.com";
 
-        User user = new User(nickname, password, email);
-        assertThat(user).isEqualTo(new User("soon", "1234", "soon@test.com"));
+        User user = new User(userId, nickname, password, email);
+        assertThat(user).isEqualTo(new User("soonId", "soon", "1234", "soon@test.com"));
     }
 
     @Test
     @DisplayName("이메일 @ && .이 포함되어 있지않을떄 예외처리 테스트")
     void userEmailWithNoAtAndDot() {
+        String userId = "soonId";
         String nickname = "soon";
         String password = "1234";
         String email = "soontestcom";
 
         assertThatThrownBy(() -> {
-            new User(nickname, password, email);
+            new User("soonId", nickname, password, email);
         }).isInstanceOf(RequestException.class);
     }
 }
