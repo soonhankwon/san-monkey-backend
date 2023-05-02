0d385e71acfdf8f0b62f19a87279b5febe8bd3a1
soonable@naver.com <soonable@naver.com>
2023-05-02 11:44:57 +0900

    feat : 로그인 시 유효하지 않은 값 에러코드

--- src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
+++ src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     EMAIL_WITH_INVALID_EXPRESSION(HttpStatus.BAD_REQUEST, "이메일은 @와 .이 포함되어야 합니다."),
-    PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "잘못된 패스워드 입니다.");
+    PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "잘못된 패스워드 입니다."),
+    LOGIN_INVALID_VALUE(HttpStatus.BAD_REQUEST, "이메일 또는 패스워드가 일치하지 않습니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
