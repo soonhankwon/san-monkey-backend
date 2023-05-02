4f622b93f18edba10c850b17a449f28e00dcaf14
soonable@naver.com <soonable@naver.com>
2023-05-02 15:25:35 +0900

    feat : 중복 이메일 에러코드 추가

--- src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
+++ src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     EMAIL_WITH_INVALID_EXPRESSION(HttpStatus.BAD_REQUEST, "이메일은 @와 .이 포함되어야 합니다."),
     PASSWORD_INVALID(HttpStatus.BAD_REQUEST, "잘못된 패스워드 입니다."),
-    LOGIN_INVALID_VALUE(HttpStatus.BAD_REQUEST, "이메일 또는 패스워드가 일치하지 않습니다.");
+    LOGIN_INVALID_VALUE(HttpStatus.BAD_REQUEST, "이메일 또는 패스워드가 일치하지 않습니다."),
+    EXISTS_USER_BY_EMAIL(HttpStatus.BAD_REQUEST, "가입된 이메일이 존재합니다.");
     private final HttpStatus httpStatus;
     private final String message;
 }
