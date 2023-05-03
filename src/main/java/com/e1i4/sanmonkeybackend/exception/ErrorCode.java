4407b7d2dfec68ea0e905edc0ac7e7f11cc1c6c3
soonable@naver.com <soonable@naver.com>
2023-05-03 12:15:31 +0900

    chore : 개행

--- src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
+++ src/main/java/com/e1i4/sanmonkeybackend/exception/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     EXISTS_USER_BY_EMAIL(HttpStatus.BAD_REQUEST, "가입된 이메일이 존재합니다."),
     USERID_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 아이디가 존재하지 않습니다."),
     SAME_PASSWORD(HttpStatus.BAD_REQUEST, "기존 비밀번호와 동일한 비밀번호 입니다.");
+
     private final HttpStatus httpStatus;
     private final String message;
 }
