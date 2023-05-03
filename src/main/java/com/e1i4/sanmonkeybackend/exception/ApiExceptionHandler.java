d26ae4ebaf85810354fc54829471fea726f39281
soonable@naver.com <soonable@naver.com>
2023-05-03 12:31:13 +0900

    fix : RestControllerAdvice 로 수정

--- src/main/java/com/e1i4/sanmonkeybackend/exception/ApiExceptionHandler.java
+++ src/main/java/com/e1i4/sanmonkeybackend/exception/ApiExceptionHandler.java
@@ -2,10 +2,10 @@ package com.e1i4.sanmonkeybackend.exception;
 
 import com.e1i4.sanmonkeybackend.dto.GlobalResDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiExceptionHandler {
     @ExceptionHandler(value = {ApiException.class})
     public ResponseEntity<GlobalResDto> handleRequestException(ApiException e) {
